a45e44becf17dfb306f658066317fccff53ffba1
Alessandro Scandone <alescandone@gmail.com>
2022-09-03 01:28:59 +0200

    edited form

--- example/form/FormExample.elm
+++ example/form/FormExample.elm
@@ -50,33 +50,49 @@ subscriptions _ =
 
 viewCard : List (Html msg) -> Html msg
 viewCard =
-    div [ class "max-w-md px-4 py-4 w-full rounded bg-white border shadow-sm" ]
+    div [ class "max-w-md px-6 py-6 w-full rounded-lg bg-white border shadow-md" ]
 
 
 view : Model -> Html Msg
 view _ =
-    div [ class "flex items-center justify-center h-screen" ]
-        [ viewCard
-            [ h3 [ class "font-bold text-xl leading-none" ] [ text "Log in" ]
-            , div [ class "h-4" ] []
-            , Ui.Input.view
+    div [ class "flex flex-col items-center justify-center h-screen" ]
+        [ div [ class "text-center" ]
+            [ h3 [ class "font-bold text-2xl leading-none" ]
+                [ text "Sign up" ]
+            , div [ class "h-3" ] []
+            , p [ class "text-gray-700" ]
+                [ text "Already registered? "
+                , a
+                    [ class "text-cyan-600 underline cursor-pointer"
+                    ]
+                    [ text "Sign in" ]
+                , text " to your account"
+                ]
+            ]
+        , div [ class "h-10" ] []
+        , viewCard
+            [ Ui.Input.view
                 [ Ui.Input.label "Email"
-                , Ui.Input.placeholder "johndoe@example.com"
+                ]
+            , div [ class "h-4" ] []
+            , div [ class "flex gap-x-5" ]
+                [ Ui.Input.view
+                    [ Ui.Input.label "First name"
+                    ]
+                , Ui.Input.view
+                    [ Ui.Input.label "Last name"
+                    ]
                 ]
             , div [ class "h-4" ] []
             , Ui.Input.view
                 [ Ui.Input.label "Password"
-                , Ui.Input.placeholder "password"
                 , Ui.Input.type_ Ui.Input.password
                 ]
             , div [ class "h-8" ] []
-            , div [ class "flex justify-around gap-x-4" ]
-                [ Ui.Button.primary
-                    [ Ui.Button.stretch True
-                    ]
-                    "Create"
-                , Ui.Button.secondary []
-                    "Cancel"
+            , Ui.Button.primary
+                [ Ui.Button.stretch True
+                , Ui.Button.size Ui.Button.large
                 ]
+                "Create"
             ]
         ]
